6e1331d92181aeff255207bc47f82f91445c4c29
Mohamed Nuzrath <76866553+nplusplusstudios@users.noreply.github.com>
2026-06-18 22:12:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 
 st.write("Hello **world!* :sunglasses:")
 x = st.text_input("Favorite Movie?")
 st.write(f"Your Favorite Movie is : {x}")
+
+data = pd.read_csv("imdb_top_1000.csv")
