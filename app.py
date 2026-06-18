e43d86aeb595594dcb71d62ea676aa9dcc5cfb13
Mohamed Nuzrath <76866553+nplusplusstudios@users.noreply.github.com>
2026-06-18 22:26:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
-st.write("Hello **world!* :sunglasses:")
-x = st.text_input("Favorite Movie?")
-st.write(f"Your Favorite Movie is : {x}")
+#st.write("Hello **world!* :sunglasses:")
+#x = st.text_input("Favorite Movie?")
+#st.write(f"Your Favorite Movie is : {x}")
 
-data = pd.read_csv("imdb_top_1000.csv")
-st.write(data)
+#data = pd.read_csv("imdb_top_1000.csv")
+#st.write(data)
+
+st.write("# My Cool Chart")
+
+chart_data = pd.DataFrame(
+  np.random.randn(20,3),
+  columns=["a","b","c"]
+)
+
+st.bar_chart(chart_data)
+st.line_chart(chart_data)
+
+
