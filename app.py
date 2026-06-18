65c07842caa04db95bda42178bcfa0066c61b7d6
Mohamed Nuzrath <76866553+nplusplusstudios@users.noreply.github.com>
2026-06-18 22:05:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.write("Hello **world!* :thumbsup:")
+st.write("Hello **world!* :sunglasses:")
 x = st.text_input("Favorite Movie?")
 st.write(f"Your Favorite Movie is : {x}")
